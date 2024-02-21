9fb3d42b38988bf9db45c3c52874872dd8e17e37
Suhaib Gour <76823175+goriens@users.noreply.github.com>
2024-02-21 18:37:03 +0530

    set up sign up

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import { AllRoutes } from "./pages/AllRoutes";
-import { Signup } from "./pages/user/Signup";
 
 function App() {
   return (
     <>
       <AllRoutes />
-      <Signup />
     </>
   );
 }
